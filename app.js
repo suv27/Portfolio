d0bdbe499584e410cebea15fb84dd4817a3e3bf4
NSTF <starlinu33@gmail.com>
2018-08-05 15:25:09 -0400

    started everything over, got my initial workflow

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require(`express`);
 const app = express();
-const port = 3000;
+const port = 4000;
 const { Pool, Client } = require('pg');
 const parser = require('body-parser');
 app.use(parser.urlencoded({ extended: true }));
