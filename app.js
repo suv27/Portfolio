72e7388100a4fd4a31061d0dd041475456244be7
NSTF <starlinu33@gmail.com>
2018-08-06 00:43:40 -0400

    re-wrote the home route name

--- app.js
+++ app.js
@@ -8,23 +8,10 @@ app.use(express.json());
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-// const pool = new Pool({
-//     user: 'postgres',
-//     host: 'localhost',
-//     database: 'postgres',
-//     password: '#BeastMode27',
-//     port: 5432,
-// });
-
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: true,
-// });
-
 let newObj = {};
 
 app.get(`/`, (req, res) => {
-  res.render('index', {
+  res.render('home', {
 
   });
 });
